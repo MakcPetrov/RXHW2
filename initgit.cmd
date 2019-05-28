dc02a178fdbc1a17f048b0b75670a10afef2202f
MakcPetrov <k-113@yandex.ru>
2019-05-28 17:52:39 +0300

    1

---
+++ initgit.cmd
@@ -0,0 +1,10 @@
+echo "# RXHW2" >> README.md
+"C:\Program Files\SmartGit\git\bin\git" init
+"C:\Program Files\SmartGit\git\bin\git" add README.md
+"C:\Program Files\SmartGit\git\bin\git" commit -m "first commit"
+"C:\Program Files\SmartGit\git\bin\git" remote add origin https://github.com/MakcPetrov/RXHW2.git
+"C:\Program Files\SmartGit\git\bin\git" push -u origin master
+
+
+
+
